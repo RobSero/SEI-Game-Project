180caa1a2470405d4e20541941ee59b5e73fc4f8
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-04-12 13:39:44 +0100

    stored html elements in memory via id

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,19 @@
 function init() {
  
-  const grid = document.querySelector('.grid')
   const width = 10
   const cellCount = width * width
   const cells = []
   const wallCells = [0,1,2,3,4,5,6,7,8,9,10,19,20,22,23,24,26,27,29,30,32,33,34,36,37,39,40,49,50,52,53,54,55,56,57,59,60,62,63,64,65,66,67,69,70,72,73,74,75,76,77,79,80,89,90,91,92,93,94,95,96,97,98,99]
   const scoreDisplay = document.getElementById('score-display')
-  const body = document.body
+ 
 
+  // ------------------------------------------------------- ELEMENTS  -------------------------------------- //
+
+
+  const grid = document.querySelector('.grid')
+  const startButton = document.getElementById('start')
+  const scoreDisplay = document.getElementById('score-display')
+  const body = document.body
 
   // -------------------------------------------------------CREATE BOARD -------------------------------------- //
 
