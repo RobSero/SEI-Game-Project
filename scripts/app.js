07f9a04a0c0e7e4dd5f5386f60b0a7bf714fb770
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-04-12 10:24:47 +0100

    JS added to build grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,33 @@
 function init() {
-  alert('yo')
+ 
+  const grid = document.querySelector('.grid')
+  const width = 10
+  const cellCount = width * width
+  const cells = []
+
+  const scoreDisplay = document.getElementById('score-display')
+  const body = document.body
+
+
+  // -------------------------------------------------------CREATE BOARD -------------------------------------- //
+
+  function createCells(){
+    for (let i = 0; i < cellCount; i++){
+      const cell = document.createElement('div')
+      cell.setAttribute('id', i)
+      cell.textContent = i
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+  }
+
+  createCells()
+
+
+
+
+
+
 }
 
 window.addEventListener('DOMContentLoaded', init)
