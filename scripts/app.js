06c733dfa6f9e4e4bebf039c3d3aa83e9d34e386
Rob Serowka <robert.serowka@hotmail.co.uk>
2020-04-13 18:19:54 +0100

    added second trap and refactored

--- scripts/app.js
+++ scripts/app.js
@@ -10,7 +10,7 @@ function init() {
   const cellCount = width * width
   const cells = []
   const wallCells = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,29,30,31,35,39,40,42,44,45,46,47,53,57,59,60,67,69,70,71,73,75,76,77,79,80,82,84,85,89,90,91,99,100,102,104,105,109,113,115,116,117,119,120,122,126,127,131,132,133,139,140,142,144,148,19,150,154,156,157,158,159,160,164,166,179,180,182,184,186,188,189,190,191,193,194,195,196,199,200,204,206,208,209,210,211,215,216,219,220,222,223,226,233,239,240,242,243,249,255,257,258,259,260,262,266,267,269,271,272,273,275,279,280,282,284,286,289,291,292,293,295,297,299,300,306,308,319,320,322,323,324,330,331,332,333,335,336,337,339,340,342,343,344,346,347,348,355,356,357,359,360,370,371,372,379,380,381,382,383,384,385,386,387,388,389,390,391,392,393,394,395,396,397,398,399]
-  const fruitCells = [22,23,24,25,26,27,28,32,33,34,36,37,41,43,48,49,50,51,52,54,55,56,58,61,62,63,64,65,66,68,72,74,78,81,83,86,87,88,92,93,94,95,96,97,98,101,103,106,107,108,110,111,112,114,118,121,123,124,125,128,129,130,134,135,136,137,138,141,145,146,147,149,151,152,153,155,161,162,163,165,167,168,169,170,171,172,173,174,175,176,177,178,183,185,187,192,197,201,202,203,205,207,212,213,214,217,218,221,224,225,227,228,229,230,231,232,234,235,236,237,238,241,244,245,246,247,248,250,251,252,253,254,256,261,263,264,265,268,270,274,276,277,278,281,283,285,287,288,290,294,296,298,301,302,303,304,305,307,309,310,311,312,313,314,315,316,317,318,321,325,326,327,328,329,334,338,341,345,349,350,351,352,353,354,358,362,363,364,365,366,367,368,369,373,374,375,376,377]
+  const fruitCells = [22,23,24,25,26,27,28,32,33,34,36,37,41,43,48,49,50,51,52,54,55,56,58,61,62,63,64,65,66,68,72,74,78,81,83,86,87,88,92,93,94,95,96,97,98,101,103,106,107,108,110,111,112,114,118,121,123,124,125,128,129,130,134,135,136,137,138,141,145,146,147,149,151,152,153,155,161,162,163,165,167,168,169,170,171,173,174,175,176,177,178,183,185,187,192,197,201,202,203,205,207,212,213,214,217,218,221,224,225,227,228,229,230,231,232,234,235,236,237,238,241,244,245,246,247,248,250,251,252,253,254,256,261,263,264,265,268,270,274,276,277,278,281,283,285,287,288,290,294,296,298,301,302,303,304,305,307,309,310,311,312,313,314,315,316,317,318,321,325,326,327,328,329,334,338,341,345,349,350,351,352,353,354,358,362,363,364,365,366,367,368,369,373,374,375,376,377]
   const bigFruitCells = [21,38,198,361,378]
   const startingPostion = 350
   let score = 0
@@ -156,57 +156,54 @@ function init() {
   // -------------------------------------------------------CREATE BOARD -------------------------------------- //
 
   function buildBoard() {
-
-    function createCells(){
-      for (let i = 0; i < cellCount; i++){
-        const cell = document.createElement('div')
-        cell.setAttribute('id', i)
-        // cell.textContent = i
-        grid.appendChild(cell)
-        cells.push(cell)
-      }
-    }
-
-    // ----- ADD BUILD WALLS ON GRID
-    function addCellWalls(){
-      wallCells.forEach(item => {
-        cells[item].classList.add('walls')
-      })
-    }
-
-    // ----- ADD FRUIT TO BOARD
-    function addFruitToGrid(){
-      fruitCells.forEach(fruit => {
-        cells[fruit].classList.add('fruit')
-      })
-      cells[startingPostion].classList.remove('fruit')
-    }
-    
-
-    // ----- ADD BIG FRUIT TO GRID
-    function addBigFruitToGrid() {
-      bigFruitCells.forEach(bigFruit => {
-        cells[bigFruit].classList.add('bigFruit')
-      })
-    }
-
-    function addTrapOne() {
-      cells[181].classList.add('trapOne')
-    }
-    
-
-
-
-
     createCells()
     addCellWalls()
     addFruitToGrid()
     addBigFruitToGrid()
     addTrapOne()
+    addTrapTwo()
     setInterval(checkForTrapTrigger,20)
     setInterval(checkForFireDeath,20)
 
+  }
 
+  function createCells(){
+    for (let i = 0; i < cellCount; i++){
+      const cell = document.createElement('div')
+      cell.setAttribute('id', i)
+      // cell.textContent = i
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+  }
+
+  // ----- ADD BUILD WALLS ON GRID
+  function addCellWalls(){
+    wallCells.forEach(item => {
+      cells[item].classList.add('walls')
+    })
+  }
+
+  // ----- ADD FRUIT TO BOARD
+  function addFruitToGrid(){
+    fruitCells.forEach(fruit => {
+      cells[fruit].classList.add('fruit')
+    })
+    cells[startingPostion].classList.remove('fruit')
+  }
+  // ----- ADD BIG FRUIT TO GRID
+  function addBigFruitToGrid() {
+    bigFruitCells.forEach(bigFruit => {
+      cells[bigFruit].classList.add('bigFruit')
+    })
+  }
+
+  function addTrapOne() {
+    cells[181].classList.add('trapOne')
+  }
+
+  function addTrapTwo() {
+    cells[172].classList.add('trapTwo')
   }
   
   // -------------------------------------------------------PACMAN OBJECT AND MOVEMENT -------------------------------------- //
@@ -232,29 +229,31 @@ function init() {
   // ------------------------------------------- VALIDATE KEY PRESS AND UPDATE PACMAN MOVE -------------------------------------- 
 
   function validatePress(event){
-    let moveDirection
-    switch (event.keyCode){
-      case 38:
-        moveDirection = -width
-        break
-      case 40:
-        moveDirection = width
-        break
-      case 39:
-        moveDirection = 1
-        break
-      case 37:
-        moveDirection = -1
-        break
-      default:
-        return
-    }
+    if (playing){
+      let moveDirection
+      switch (event.keyCode){
+        case 38:
+          moveDirection = -width
+          break
+        case 40:
+          moveDirection = width
+          break
+        case 39:
+          moveDirection = 1
+          break
+        case 37:
+          moveDirection = -1
+          break
+        default:
+          return
+      }
 
-    if (wallCells.includes(pacman.positionDivNo + moveDirection)){
-      return
-    } else {
-      if (moveDirection !== pacman.move){
-        pacman.move = moveDirection
+      if (wallCells.includes(pacman.positionDivNo + moveDirection)){
+        return
+      } else {
+        if (moveDirection !== pacman.move){
+          pacman.move = moveDirection
+        }
       }
     }
   }
@@ -263,27 +262,29 @@ function init() {
   // ------------------------------------------- PACMAN EATS NORMAL FRUIT -------------------------------------- 
 
   function eatFruit(){
-
-    if (cells[pacman.positionDivNo].classList.contains('fruit')){
-      score += 100
-      cells[pacman.positionDivNo].classList.remove('fruit')
-      scoreDisplay.innerHTML = score
-    } else {
-      cells[pacman.positionDivNo].classList.remove('fruit')
+    if (playing){
+      if (cells[pacman.positionDivNo].classList.contains('fruit')){
+        score += 100
+        cells[pacman.positionDivNo].classList.remove('fruit')
+        scoreDisplay.innerHTML = score
+      } else {
+        cells[pacman.positionDivNo].classList.remove('fruit')
+      }
     }
   }
 
   // ------------------------------------------- PACMAN EATS BIG FRUIT -------------------------------------- 
 
   function CheckForBigFruitEaten(){
-
-    if (cells[pacman.positionDivNo].classList.contains('bigFruit')){
-      score += 500
-      cells[pacman.positionDivNo].classList.remove('bigFruit')
-      scoreDisplay.innerHTML = score
-      bigFruitEaten()
-    } else {
-      cells[pacman.positionDivNo].classList.remove('bigFruit')
+    if (playing){
+      if (cells[pacman.positionDivNo].classList.contains('bigFruit')){
+        score += 500
+        cells[pacman.positionDivNo].classList.remove('bigFruit')
+        scoreDisplay.innerHTML = score
+        bigFruitEaten()
+      } else {
+        cells[pacman.positionDivNo].classList.remove('bigFruit')
+      }
     }
   }
 
@@ -316,30 +317,31 @@ function init() {
   // ------------------------------------------- MOVE GHOST  -------------------------------------- 
 
   function moveGhost(ghost){
-    if (ghost.positionDivNo === ghostHome && ghost.state === 'eaten'){
-      homeTimer(ghost)
-      ghost.state = 'waiting'
-      return
-    }
-    if (ghost.state === 'waiting'){
-      return
-    }
-    ghost.upperDiv = ghost.positionDivNo - width
-    ghost.lowerDiv = ghost.positionDivNo + width
-    ghost.leftDiv = ghost.positionDivNo - 1
-    ghost.rightDiv = ghost.positionDivNo + 1
-    calculateSurroundingDistances(ghost)
-    findShortestRoute(ghost)
+    if (playing){
+      if (ghost.positionDivNo === ghostHome && ghost.state === 'eaten'){
+        homeTimer(ghost)
+        ghost.state = 'waiting'
+        return
+      }
+      if (ghost.state === 'waiting'){
+        return
+      }
+      ghost.upperDiv = ghost.positionDivNo - width
+      ghost.lowerDiv = ghost.positionDivNo + width
+      ghost.leftDiv = ghost.positionDivNo - 1
+      ghost.rightDiv = ghost.positionDivNo + 1
+      calculateSurroundingDistances(ghost)
+      findShortestRoute(ghost)
     
     
-    cells[ghost.positionDivNo].classList.remove(ghost.name)
-    cells[ghost.positionDivNo].classList.remove('scaredGhost')
-    cells[ghost.positionDivNo].classList.remove('eatenGhost')
-    ghost.prevDivNo = ghost.positionDivNo
-    ghost.positionDivNo += ghost.move
-    cells[ghost.positionDivNo].classList.add( ghostIcon(ghost))
+      cells[ghost.positionDivNo].classList.remove(ghost.name)
+      cells[ghost.positionDivNo].classList.remove('scaredGhost')
+      cells[ghost.positionDivNo].classList.remove('eatenGhost')
+      ghost.prevDivNo = ghost.positionDivNo
+      ghost.positionDivNo += ghost.move
+      cells[ghost.positionDivNo].classList.add( ghostIcon(ghost))
 
-
+    }
     function ghostIcon(ghost) {
       if (ghost.state === 'scared'){
         return 'scaredGhost'
@@ -458,34 +460,37 @@ function init() {
 
   
   function toggleChase(isChasing){
-   
-    if (isChasing){
-      clearInterval(chasing)
-      chase = false
-      scattering = setInterval(scatterGhostPath, 150)
+    if (playing){
+      if (isChasing){
+        clearInterval(chasing)
+        chase = false
+        scattering = setInterval(scatterGhostPath, 150)
       // body.style.backgroundColor = 'green'
-    } else {
-      clearInterval(scattering)
-      chase = true
-      chasing = setInterval(ghostChasePath, 500)
+      } else {
+        clearInterval(scattering)
+        chase = true
+        chasing = setInterval(ghostChasePath, 500)
       // body.style.backgroundColor = 'pink'
+      }
     }
   }
 
   function ghostChasePath() {
-    if (greenGhost.state === 'normal'){
-      greenGhost.target = pacman.positionDivNo
-    }
-    if (redGhost.state === 'normal'){
-      redGhost.target = greenGhost.target - 11
-    }
-    if (yellowGhost.state === 'normal'){
-      yellowGhost.target = pacman.positionDivNo + pacman.move
-    }
-    if (pinkGhost.state === 'normal'){
-      pinkGhost.target = pacman.positionDivNo + 10
-    }
+    if (playing){
+      if (greenGhost.state === 'normal'){
+        greenGhost.target = pacman.positionDivNo
+      }
+      if (redGhost.state === 'normal'){
+        redGhost.target = greenGhost.target - 11
+      }
+      if (yellowGhost.state === 'normal'){
+        yellowGhost.target = pacman.positionDivNo + pacman.move
+      }
+      if (pinkGhost.state === 'normal'){
+        pinkGhost.target = pacman.positionDivNo + 10
+      }
     // console.log(greenGhost.target)
+    }
   }
 
   function scatterGhostPath(){
@@ -518,13 +523,15 @@ function init() {
 
   
   function checkIfEaten(){
-    ghosts.forEach(ghost => {
-      if (pacman.positionDivNo === ghost.positionDivNo && ghost.state === 'scared'){
-        score += 500
-        ghost.state = 'eaten'
-        console.log(`${ghost.name} State: ${ghost.state}`)
-      }
-    })
+    if (playing){
+      ghosts.forEach(ghost => {
+        if (pacman.positionDivNo === ghost.positionDivNo && ghost.state === 'scared'){
+          score += 500
+          ghost.state = 'eaten'
+          console.log(`${ghost.name} State: ${ghost.state}`)
+        }
+      })
+    }
   }
 
   const eatenCheck = setInterval(checkIfEaten,10)
@@ -541,19 +548,23 @@ function init() {
 
 
   function checkForWin() {
-    if (cells.every(cell => {
-      return !(cell.classList.contains('fruit'))
-    })) {
-      alert('you win!')
-    } 
+    if (playing){
+      if (cells.every(cell => {
+        return !(cell.classList.contains('fruit'))
+      })) {
+        alert('you win!')
+      }
+    }
   }
 
   function checkForLose() {
-    ghosts.forEach(ghost => {
-      if (pacman.positionDivNo === ghost.positionDivNo && ghost.state === 'normal') {
-        alert('you lose!')
-      }
-    })
+    if (playing){
+      ghosts.forEach(ghost => {
+        if (pacman.positionDivNo === ghost.positionDivNo && ghost.state === 'normal') {
+          alert('you lose!')
+        }
+      })
+    }
   }
 
   // setInterval(checkForLose, 50)
@@ -562,57 +573,69 @@ function init() {
   // ------------------------------------------- TRAP ONE TEST -------------------------------------- 
 
   function checkForTrapTrigger(){
-    if (cells[pacman.positionDivNo].classList.contains('trapOne')){
-      cells[181].classList.remove('trapOne')
-      console.log('activate once')
-      fireTrapActivate()
-      
+    if (playing){
+      if (cells[pacman.positionDivNo].classList.contains('trapOne')){
+        cells[181].classList.remove('trapOne')
+        console.log('activate once')
+        fireTrapActivate(21,361,20)
+        setTimeout(addTrapOne, 11000)
+      }
+      if (cells[pacman.positionDivNo].classList.contains('trapTwo')){
+        cells[172].classList.remove('trapTwo')
+        console.log('activate once')
+        fireTrapActivate(167,178,1)
+        setTimeout(addTrapTwo, 11000)
+      }
     }
   }
 
-  function fireTrapActivate() {
-    let topFirePosition = 21
-    let bottomFirePosition = 361
-    cells[topFirePosition].classList.add('fire')
-    cells[bottomFirePosition].classList.add('fire')
+  function fireTrapActivate(fireStart,fireEnd,fireDirection) {
+    
+    cells[fireStart].classList.add('fire')
+    cells[fireEnd].classList.add('fire')
 
     const fireTrap = setInterval(() => {
-      topFirePosition += 20
-      bottomFirePosition -= 20
-      cells[topFirePosition].classList.add('fire')
-      cells[bottomFirePosition].classList.add('fire')
-      if (topFirePosition === 181){
+      fireStart += fireDirection
+      fireEnd -= fireDirection
+      cells[fireStart].classList.add('fire')
+      cells[fireEnd].classList.add('fire')
+      if (fireStart === 201){
         clearInterval(fireTrap)
-        removeFire()
+        removeFire(21,361,fireDirection)
+      }
+      if (fireStart === 173){
+        clearInterval(fireTrap)
+        removeFire(167,178,fireDirection)
       }
     },150)
   }
 
-  function removeFire(){
-    let topFire = 21
-    let bottomFire = 361
-    cells[topFire].classList.remove('fire')
-    cells[bottomFire].classList.remove('fire')
+  function removeFire(fireStart,fireEnd,fireDirection){
+    
+    cells[fireStart].classList.remove('fire')
+    cells[fireEnd].classList.remove('fire')
     const removeFireTrap = setInterval(() => {
-      topFire += 20
-      bottomFire -= 20
-      cells[topFire].classList.remove('fire')
-      cells[bottomFire].classList.remove('fire')
-      if (topFire === 181){
+      fireStart += fireDirection
+      fireEnd -= fireDirection
+      cells[fireStart].classList.remove('fire')
+      cells[fireEnd].classList.remove('fire')
+      if (fireStart === 181 || fireStart === 173){
         clearInterval(removeFireTrap)
       }
     },200)
   }
 
   function checkForFireDeath(){
-    if (cells[pacman.positionDivNo].classList.contains('fire')){
+    if (playing){
+      if (cells[pacman.positionDivNo].classList.contains('fire')){
       // alert('you burned!')
-    }
-    ghosts.forEach(ghost => {
-      if (cells[ghost.positionDivNo].classList.contains('fire')){
-        ghost.state = 'eaten'
       }
-    })
+      ghosts.forEach(ghost => {
+        if (cells[ghost.positionDivNo].classList.contains('fire')){
+          ghost.state = 'eaten'
+        }
+      })
+    }
   }
 
  
@@ -623,6 +646,8 @@ function init() {
   startButton.addEventListener('click', () => {
     if (!playing) {
       startGame()
+      grid.style.visibility = 'visible'
+      startButton.style.visibility = 'hidden'
       window.addEventListener('keydown', validatePress)
     }
   })
